1d8a4811b577b1b4187d4a5d427fd4845b1de13c
Asta86 <psljp@protonmail.com>
2023-07-29 19:39:14 -0300

    Update curse names

--- src/chiv-auto-protect.py
+++ src/chiv-auto-protect.py
@@ -1,15 +1,16 @@
 from Scripts.src.utils import *
 
-curses = {"Bload Oath", "Clumsy", "Corpse Skin", "Curse", "Evil Omen", "Feeble Mind", "Mind Rot", "Mortal Strike",
+curses = {"Bload Oath (curse)", "Clumsy", "Corpse Skin", "Curse", "Evil Omen", "Feeble Mind", "Mind Rot", "Mortal Strike",
                "Paralyze", "Strangle", "Weaken"}
 fcr_delay = 1600
+hpdiff = 20
 
 while not Player.IsGhost:
     if Player.Poisoned and (Player.Mana >= 10):
         cast("Cleanse by Fire", True)
         Misc.Pause(fcr_delay)
     if Player.Poisoned: continue
-    if (Player.Hits <= 100) and (Player.Mana >= 10):
+    if (Player.Hits <= (Player.HitsMax - hpdiff)) and (Player.Mana >= 10):
         cast("Close Wounds", True)
         Misc.Pause(fcr_delay)
     if any(map(Player.BuffsExist, curses)) and (Player.Mana >= 20):
