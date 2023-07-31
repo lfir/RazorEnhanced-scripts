ce18070f099e77a588edb47bbad959f1d8f1f947
Asta86 <psljp@protonmail.com>
2023-07-31 13:51:00 -0300

    Clear warnings

--- src/tame-closest-target.py
+++ src/tame-closest-target.py
@@ -1,24 +1,26 @@
 from AutoComplete import *
 
-def findMobile():
-    mobileFilter = Mobiles.Filter()
-    mobileFilter.RangeMax = 2
-    mobileFilter.Notorieties.Add(1)  # 1: blue
-    mobileFilter.Notorieties.Add(2)  # 2: green
-    mobileFilter.Notorieties.Add(3)  # 3: gray, neutral
-    mobileFilter.Notorieties.Add(4)  # 4: gray, criminal
-    mobileFilter.CheckIgnoreObject = True
-    mobileFilter.CheckLineOfSight = True
-    mobiles = Mobiles.ApplyFilter(mobileFilter)
-    return mobiles
+
+def find_mobile():
+    mobilefilter = Mobiles.Filter()
+    mobilefilter.RangeMax = 2
+    mobilefilter.Notorieties.Add(1)  # 1: blue
+    mobilefilter.Notorieties.Add(2)  # 2: green
+    mobilefilter.Notorieties.Add(3)  # 3: gray, neutral
+    mobilefilter.Notorieties.Add(4)  # 4: gray, criminal
+    mobilefilter.CheckIgnoreObject = True
+    mobilefilter.CheckLineOfSight = True
+    mobiles = Mobiles.ApplyFilter(mobilefilter)
+    return Mobiles.Select(mobiles, "Nearest")
 
 
 Journal.Clear()
 while not Player.IsGhost:
     if Journal.SearchByType("You must wait a few moments to use another skill.", "System"):
         break
-    if Target.HasTarget: Target.Cancel()
+    if Target.HasTarget:
+        Target.Cancel()
     Player.UseSkill("Animal Taming")
     Target.WaitForTarget(2000, True)
-    Target.TargetExecute(Mobiles.Select(findMobile(), "Nearest"))
+    Target.TargetExecute(find_mobile())
     Misc.Pause(100)
