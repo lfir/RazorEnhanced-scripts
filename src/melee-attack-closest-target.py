2d6e819497edef44a86b3fc654cbb4d9ae095b82
Asta86 <psljp@protonmail.com>
2023-08-28 19:31:53 -0300

    Order enemies by distance using sorted function to improve readability

--- src/melee-attack-closest-target.py
+++ src/melee-attack-closest-target.py
@@ -2,8 +2,8 @@ from Scripts.src.utils import *
 
 while not Player.IsGhost:
     enemies = find_mobiles(range(3, 7), 5)
-    Mobiles.Select(enemies, "Nearest")
-    for enemy in enemies:
+    enemies_by_dist = sorted(enemies, key=lambda e: Player.DistanceTo(e))
+    for enemy in enemies_by_dist:
         target = Mobiles.FindBySerial(enemy.Serial)
         while target and Player.InRangeMobile(target, 2):
             if Player.BuffsExist(curses[0]):
